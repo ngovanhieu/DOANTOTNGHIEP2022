670b450ad55eda8f52d0d48fa8b01e6e1b28e1b2
ngovanhieu <hieungo30092703@gmail.com>
2022-11-07 16:43:16 +0700

    test

--- src/component/blog/BlogDetail.js
+++ src/component/blog/BlogDetail.js
@@ -2,6 +2,12 @@ function BlogDetail(){
     return(
         <>
             <h1>tesst detail</h1>
+            <div>
+                <p>
+                    tesst 
+                    
+                </p>
+            </div>
         </>
     )
 }
